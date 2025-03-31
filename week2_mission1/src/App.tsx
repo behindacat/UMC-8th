bcfa9348030d75746d8416dfcb48daf61819b063
behindacat <yulmooop@gmail.com>
2025-03-31 11:26:35 +0900

    week2 mission1

---
+++ week2_mission1/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Todo from './components/Todo';
+// import { TodoProvider } from './context/TodoContext';
+
+function App() : Element {
+    return (
+        <>
+            <Todo />
+        </>;
+    );
+}
+
+export default App;
