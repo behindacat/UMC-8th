7c17bba9bc030c2712bd23512d8c168930f45c0f
behindacat <yulmooop@gmail.com>
2025-04-02 00:00:38 +0900

    week2 mission1 수정

--- week2_mission1/src/App.tsx
+++ week2_mission1/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Todo from './components/Todo';
-// import { TodoProvider } from './context/TodoContext';
+import { TodoProvider } from './context/TodoContext';
 
 function App() : Element {
     return (
-        <>
+        <TodoProvider>
             <Todo />
-        </>;
+        </TodoProvider>
     );
 }
 
