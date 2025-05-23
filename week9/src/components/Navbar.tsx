8e84fe91c9e40e32bb9d486a1d74d848ece08391
behindacat <yulmooop@gmail.com>
2025-05-24 02:52:53 +0900

    week9 mission1

---
+++ week9/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "../hooks/useCustomRedux";
+import { useDispatch } from "../hooks/useCustomRedux";
+import { useEffect } from "react";
+import { calculateTotals } from "../slices/cartSlice";
+
+const Navbar = () => {
+   const { amount, cartItems } = useSelector((state) => state.cart);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(calculateTotals());
+   }, [dispatch, cartItems]);
+   
+   return(
+      <div className='flex justify-between items-center p-4 bg-gray-800 text-white'>
+         <h1 
+            onClick={() => {
+               window.location.href = '/';
+            }}
+            className='text-2xl font-semibold'
+         >
+            Ohthani Ahn
+         </h1>
+         <div className='flex items-center space-x-2'>
+            <FaShoppingCart className='text-2xl' />
+            <span className='text-xl font-medium'>{amount}</span>
+         </div>
+      </div>
+   );
+};
+
+export default Navbar;
