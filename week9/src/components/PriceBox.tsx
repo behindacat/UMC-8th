4b9d0e2d15e6ee88daf0c9b797978c64abc06852
behindacat <yulmooop@gmail.com>
2025-05-30 15:20:56 +0900

    week9 미션 가격 기능 수정

--- week9/src/components/PriceBox.tsx
+++ week9/src/components/PriceBox.tsx
@@ -1,12 +1,13 @@
-import { useDispatch, useSelector } from "../hooks/useCustomRedux";
+import { useDispatch } from "../hooks/useCustomRedux";
 import { openModal } from "../slices/modalSlice";
+import { useCartInfo } from "../hooks/useCartStore";
 
 const PriceBox = () => {
-   const { total } = useSelector((state) => state.cart);
-   const dispatch = useDispatch();
+   const { total } = useCartInfo(); // Zustand에서 cart 정보 가져오기
+   const dispatch = useDispatch();  // Redux dispatch는 그대로 모달용
 
    const handleInitializeCart = () => {
-      dispatch(openModal());
+      dispatch(openModal()); // 모달 열기 (Redux 상태)
    };
 
    return (
@@ -15,8 +16,8 @@ const PriceBox = () => {
             onClick={handleInitializeCart}
             className='border p-4 rounded-md cursor-pointer'>
                전체 삭제
-            </button>
-            <div>총 가격: {total}원</div>
+         </button>
+         <div>총 가격: {total}원</div>
       </div>
    );
 };
